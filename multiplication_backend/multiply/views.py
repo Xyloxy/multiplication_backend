c3cc6caf39bd991a53c7047d03cc4a340fc233f0
Xylox <seba15833@gmail.com>
2024-11-11 20:12:28 +0100

    Formatting + requirements + readme

--- multiplication_backend/multiply/views.py
+++ multiplication_backend/multiply/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
 class MultiplyView(APIView):
     def post(self, request):
@@ -8,17 +9,19 @@ class MultiplyView(APIView):
         data = request.data
         number1 = data.get("number1", None)
         number2 = data.get("number2", None)
-        
+
         if number1 is None or number2 is None:
             return Response({"error": "number1 and number2 are required"}, status=400)
-        
+
         # Parse data
         try:
             number1 = float(number1)
             number2 = float(number2)
         except (ValueError, TypeError):
-            return Response({"error": "number1 and number2 must be integers or floats"}, status=400)
-        
+            return Response(
+                {"error": "number1 and number2 must be integers or floats"}, status=400
+            )
+
         result = number1 * number2
-        
+
         return Response({"result": result})
--- multiplication_backend/urls.py
+++ multiplication_backend/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from multiplication_backend.multiply.views import MultiplyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('multiply/?$', MultiplyView.as_view())
+    re_path('multiply/?$', MultiplyView.as_view())
 ]
